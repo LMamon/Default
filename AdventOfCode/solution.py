cffcbb046ac8f05e7cb5ed5b2360b0c62fe7ea98
LMamon <knit-close.0o@icloud.com>
2023-12-04 19:21:28 -0900

    test v5

--- AdventOfCode/solution.py
+++ AdventOfCode/solution.py
@@ -6,10 +6,14 @@ lines = file_code.splitlines()
 number = 0
 int_position_in_line = []
 for line in lines:
-    text_in_line = [int(char) for char in line if char.isdigit()]
-    print(line, text_in_line)
-    if text_in_line:
-        number += text_in_line[0] + text_in_line[-1]
+    #text_in_line = [int(char) for char in line if char.isdigit()]
+    for char in line:
+        if char.isdigit():
+            int_position_in_line.append(int(char))
+            #print(line, text_in_line)
 
-            
+if len(int_position_in_line) >= 2:
+    number += int_position_in_line[0] + int_position_in_line[-1]
+
+print(sum(int_position_in_line))            
 print(number)
