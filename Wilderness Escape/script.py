7861d26e309676ac755c823d973dbc25f48c8fdb
Louis Mamon <knit-close0o@icloud.com>
2024-03-15 22:33:28 -0800

    getting attribute error in line 20 but should be finished

--- Wilderness Escape/script.py
+++ Wilderness Escape/script.py
@@ -3,20 +3,24 @@
 ######
 class TreeNode:
     def __init__(self, story_piece):
-        self.story_peice = story_piece
+        self.story_piece = story_piece
         self.choices = []
     def add_child(self, node):
-        self.choices += node
+        self.choices.append(node)
     def traverse(self):
         story_node = self
-        while story_node.choices:
-            print(story_node.story_peice)
+        print(story_node.story_piece)
+        while len(story_node.choices)>0:
             choice = input("Enter 1 or 2 to continue the story: ")
-            if choice != '1' and choice != '2':
+            if choice not in ["1", "2"]:
                 print("Invalid", choice)
-                continue
-            chosen_index = int(choice) - 1
-            chosen_child = story_node.choices(chosen_index)
+            else:
+                chosen_index = int(choice)
+                chosen_index -= 1
+                chosen_child = story_node.choices[chosen_index]
+                print(chosen_child.story_piece)
+                story_node = chosen_child
+        
 
 ######
 # VARIABLES FOR TREE
@@ -24,7 +28,12 @@ class TreeNode:
 story_root = TreeNode("""You are in a forest clearing. \nThere is a path to the left. \nA bear emerges from the trees and roars! Do you: \n1 ) Roar back! \n2 ) Run to the left...""")
 user_choice = input("What is your name? ")
 choice_a = """The bear is startled and runs away. Do you:\n1 ) Shout 'Sorry bear!' \n2 ) Yell 'Hooray!'"""
-choice_b = """\nYou come across a clearing full of flowers. The bear follows you and asks 'what gives?' Do you:\n1 ) Gasp 'A talking bear!'\n2 ) Explain that the bear scared you."""
+choice_b = """You come across a clearing full of flowers. The bear follows you and asks 'what gives?' Do you:\n1 ) Gasp 'A talking bear!'\n2 ) Explain that the bear scared you."""
+choice_a_1 = TreeNode("""The bear returns and tells you it's been a rough week. After making peace with \na talking bear, he shows you the way out of the forest. \nYOU HAVE ESCAPED THE WILDERNESS.""")
+choice_a_2 = TreeNode("""The bear returns and tells you that bullying is not okay before leaving you alone \nin the wilderness. \nYOU REMAIN LOST.""")
+choice_b_1 = TreeNode("""The bear is unamused. After smelling the flowers, it turns around and leaves you alone. \nYOU REMAIN LOST.""")
+choice_b_2 = TreeNode("""The bear understands and apologizes for startling you. Your new friend shows you a \npath leading out of the forest. \nYOU HAVE ESCAPED THE WILDERNESS.""")
+
 
 
 
@@ -33,7 +42,10 @@ choice_b = """\nYou come across a clearing full of flowers. The bear follows you
 #####
 print(user_choice)
 print("Once upon a time...")
-#print(story_root.story_peice)
 story_root.add_child(choice_a)
 story_root.add_child(choice_b)
 story_root.traverse()
+choice_a.add_child(choice_a_1)
+choice_a.add_child(choice_a_2)
+choice_b.add_child(choice_b_1)
+choice_b.add_child(choice_b_2)
